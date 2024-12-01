bf2e8cd3c04af84ed6b4ad35fb71a73bc6b46e0d
FelipeMaD <felipemarinhodr@hotmail.com>
2024-12-01 18:01:25 -0300

    paginação

--- valorantpixel/settings.py
+++ valorantpixel/settings.py
@@ -96,7 +96,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS" : "rest_framework.schemas.coreapi.AutoSchema" ,
     "DEFAULT_PAGINATION_CLASS" : "rest_framework.pagination.PageNumberPagination",
     "NON_FIELD_ERRORS_KEY" : "error",
-    "PAGE_SIZE" : 5
+    "PAGE_SIZE" : 10
     }
 
 SIMPLE_JWT = {
