44f90dc3b46270384a7a18947d26a3b4fe1d0543
João Dantas <joaodantastx1@gmail.com>
2024-12-02 12:59:49 -0300

    editando user nickname

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     description = models.TextField(blank=True)
-    nickname = models.CharField(max_length=50, blank=True, unique=True)
+    nickname = models.CharField(max_length=50, unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.nickname if self.nickname else self.user.username
